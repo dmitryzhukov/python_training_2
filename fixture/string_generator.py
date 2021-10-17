9db152f20b2507aed2ac0acbaa091b497528b73c
Dmitry Zhukov <dmitry.v.zhukov@mail.ru>
2021-10-17 21:11:19 +0300

    фиксы параметризации pytest

--- fixture/string_generator.py
+++ fixture/string_generator.py
@@ -4,4 +4,4 @@ import random
 
 def random_string(prefix: string, max_length: int) -> string:
     symbols = string.ascii_letters + string.digits + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.random.randrange(max_length))])
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_length))])
--- model/contact.py
+++ model/contact.py
@@ -40,7 +40,7 @@ class Contact:
         return f"{self.id}:{self.firstname}:{self.lastname}"
 
     def __eq__(self, other) -> bool:
-        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname and self.address == other.address
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
 
     def id_or_max(contact):
         if contact.id:
