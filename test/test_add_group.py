c2f1d9a6044e4a510548e0d4b5d05fc4bc2fe392
Dmitry Zhukov <dmitry.v.zhukov@mail.ru>
2021-10-23 23:40:20 +0300

    Задание №20 добавление групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,18 +5,20 @@ from model.group import Group
 from data.data import test_grops
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db,  json_groups, check_ui):
     group = json_groups
     app.group.open_groups_page()
 
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
 
     app.group.create(group)
 
     app.group.open_groups_page()
 
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
 
     assert sorted(old_groups, key=Group.id_or_max) == sorted(
         new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
