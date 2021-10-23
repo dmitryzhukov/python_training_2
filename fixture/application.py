e14a5b513044f6057b1c6f2e0baedff86266de6f
Dmitry Zhukov <dmitry.v.zhukov@mail.ru>
2021-10-23 15:30:10 +0300

    Задание №17: Реализовать загрузку информации о тестируемом приложении из конфигурационного файла

--- fixture/application.py
+++ fixture/application.py
@@ -15,6 +15,7 @@ class Application:
             self.wd = webdriver.Ie()
         else:
             raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
